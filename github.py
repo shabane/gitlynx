eda71cdbd74f0da559253b5fa8cf29c60dfbc1ce
shabane <m.mohamadshabane@gmail.com>
2023-07-22 11:41:32 +0330

    [feat](write function to list all content of / in repo):
    	this will help us figure out list of files that exist on the
    	repo, and we will create the ShortUrl html page with other name.

--- github.py
+++ github.py
@@ -21,3 +21,22 @@ def upload(token: str, file_name: str, data: str, repo: str, owner: str):
     if response.status_code != 201:
         return response
     return response
+
+def getContents(token: str, repo: str, owner: str) -> list:
+    headers = {
+        'Accept': 'application/vnd.github+json',
+        'Authorization': f'Bearer {token}',
+        'X-GitHub-Api-Version': '2022-11-28',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/contents/', headers=headers)
+    if response.status_code == 200:
+        _ = []
+        if response.content:
+            data = json.loads(response.content)
+            for i in data:
+                _.append(i['name'])
+            return json.dumps(_)
+        return _
+    raise Exception(f"there where an error to github: {response.status_code}")
